7168ccf6bdacf904866e97e72d4d13d17f5e9e1c
Geonu <geoun1109@gmail.com>
2019-09-28 08:51:28 +0900

    Initial Commit

---
+++ SwiftUI-ToDoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-ToDoList
+//
+//  Created by 전건우 on 2019/09/28.
+//  Copyright © 2019 Geonu Jeon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
